0718f966186c7a378b4f4c2b9336de9717b47e26
Heejung Yang <wolla39@gmail.com>
2025-06-25 20:59:20 +0900

    setting uikit project

---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Heejung Yang on 6/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
